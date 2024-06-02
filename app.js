ceb2241fc4be48e9139ef8e43701047c8b6cb891
AmitTomar805 <amit.s.tomar805@gmail.com>
2024-06-02 00:06:38 -0400

    Committing board pieces adding and color changes

--- app.js
+++ app.js
@@ -13,11 +13,37 @@ const startPieces=[
     rook,knight,bishop,king,queen,bishop,knight,rook,
 ]
 function createBoard(){
+    let j=0;
     startPieces.forEach((startPiece,i)=>{
         const square=document.createElement('div');
         square.innerHTML=startPiece;
         square.setAttribute('square-id',i);
-        square.classList.add('beige');
+        
+        if((i)%8==0){
+            j=j+1;
+        }
+        if(j%2==1){
+            if((i+1)%2==0){
+                square.classList.add('beige');
+            }
+            else{
+                square.classList.add('brown');
+            }
+        }
+        else{
+            if((i+1)%2==0){
+                square.classList.add('brown');
+            }
+            else{
+                square.classList.add('beige');
+            }
+        }
+        if(i<=15){
+            square.firstChild.firstChild.classList.add('black');
+        }
+        if(i>=48 && i<=63){
+            square.firstChild.firstChild.classList.add('white');
+        }
         square.classList.add('square');
         gameBoard.append(square);
     })
